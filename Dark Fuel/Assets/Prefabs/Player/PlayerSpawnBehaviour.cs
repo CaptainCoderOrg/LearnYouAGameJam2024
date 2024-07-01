b91a6738650c0b9c0efe1fa042769d9f64740ca1
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-07-01 06:37:52 -0700

    feat: Implement Spawn / Win animation

---
+++ Dark Fuel/Assets/Prefabs/Player/PlayerSpawnBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnBehaviour : StateMachineBehaviour
+{
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+    }
+}
